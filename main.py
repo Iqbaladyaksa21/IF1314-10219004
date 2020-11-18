e77c46df5e3e49c51352fc25fb4796e389ba16bd
iqbal adyaksa <73114479+Iqbaladyaksa21@users.noreply.github.com>
2020-11-18 15:50:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 class hero :
-  def __init__(self, inputName, inputHealth, inputPower, inputArmor, ):
+   jumlah = 0
+   def __init__(self, inputName, inputHealth, inputPower, inputArmor, ):
       self.name = inputName
       self.health = inputHealth
       self.power = inputPower
       self.armor = inputArmor  
+      hero.jumlah += 1
+      print("membuat hero dengan nama " + inputName)
 
-  def __del__ (self):
-    class_name =self.__class__.___name__
-    print("Sebuah Objek", class_name,", yaitu", self.name, "dihapus")
+   def __del__ (self):
+      class_name = self.__class__.__name__
+      print("Sebuah Objek", class_name,", yaitu", self.name, "dihapus")
 
 # instansiasi
 hero1 = hero("sniper",150,170,30)
+print("jumlah hero: ", hero.jumlah)
 hero2 = hero("lancelot", 250, 200, 10)
+print("jumlah hero: ", hero.jumlah)
 hero3 = hero("tiny", 500, 50, 145)
+print("jumlah hero: ", hero.jumlah)
 
 print (hero1.__dict__)
 print(hero2.__dict__)
 print(hero3.__dict__)
 
-del hero1
+del hero2
